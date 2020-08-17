c6d693dfa6b5aa83eb1710a44ada095779d77dfc
Jesse P. Johnson <jpj6652@gmail.com>
2020-08-16 23:12:18 -0400

    Bump version: 0.1.1-dev5 → 0.1.1-dev6

--- anymod/__init__.py
+++ anymod/__init__.py
@@ -5,7 +5,7 @@
 
 __author__ = 'Jesse P. Johnson'
 __title__ = 'anymod'
-__version__ = '0.1.1-dev5'
+__version__ = '0.1.1-dev6'
 __license__ = 'Apache-2.0'
 
 __all__ = ['ModuleLoader']
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,4 +2,4 @@ from anymod import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.1-dev5"
+    assert __version__ == "0.1.1-dev6"
